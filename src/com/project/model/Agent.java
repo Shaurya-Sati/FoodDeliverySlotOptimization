d3e63d8f3c0dc17e7074d0f7e2da6bdf300ed87a
Shaurya Sati <shauryasati93@gmail.com>
2026-03-21 13:20:55 +0530

    Added Order and Agent model classes

--- src/com/project/model/Agent.java
+++ src/com/project/model/Agent.java
@@ -1,4 +1,35 @@
 package com.project.model;
 
 public class Agent {
+    private int agentId;
+    private String agentName;
+    private int currentNode;
+    private boolean isAvailable;
+
+    public Agent(int agentId, String agentName, int currentNode) {
+        this.agentId = agentId;
+        this.agentName = agentName;
+        this.currentNode = currentNode;
+        this.isAvailable = true;
+    }
+
+    public int getAgentId() {
+        return agentId;
+    }
+    public String getAgentName() {
+        return agentName;
+    }
+    public int getCurrentNode() {
+        return currentNode;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setCurrentNode(int currentNode) {
+        this.currentNode = currentNode;
+    }
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
--- src/com/project/model/Order.java
+++ src/com/project/model/Order.java
@@ -1,4 +1,38 @@
 package com.project.model;
 
 public class Order {
+    private int orderId;
+    private String customerName;
+    private int restaurantNode;
+    private int deliveryNode;
+    private int deadline;
+    private int orderValue;
+
+    public Order(int orderId, String customerName, int restaurantNode, int deliveryNode, int deadline, int orderValue) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.restaurantNode = restaurantNode;
+        this.deliveryNode = deliveryNode;
+        this.deadline = deadline;
+        this.orderValue = orderValue;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public int getRestaurantNode() {
+        return restaurantNode;
+    }
+    public int getDeliveryNode() {
+        return deliveryNode;
+    }
+    public int getDeadline() {
+        return deadline;
+    }
+    public int getOrderValue() {
+        return orderValue;
+    }
 }
