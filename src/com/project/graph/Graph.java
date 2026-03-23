73ce8adf9b166a1eb9dea383e4ed7828d44f5f88
Shaurya Sati <shauryasati93@gmail.com>
2026-03-23 20:45:05 +0530

    Added Graph class

--- src/com/project/graph/Graph.java
+++ src/com/project/graph/Graph.java
@@ -1,4 +1,46 @@
 package com.project.graph;
 
+import java.util.*;
+
 public class Graph {
+
+    private int vertices; // number of nodes
+    private List<List<Edge>> adjList;
+
+    // Edge class (inner class)
+    public static class Edge {
+        int destination;
+        int weight; // distance or time
+
+        public Edge(int destination, int weight) {
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    // Constructor
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        adjList = new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    // Add edge
+    public void addEdge(int source, int destination, int weight) {
+        adjList.get(source).add(new Edge(destination, weight));
+        adjList.get(destination).add(new Edge(source, weight)); // undirected graph
+    }
+
+    // Get neighbors
+    public List<Edge> getNeighbors(int node) {
+        return adjList.get(node);
+    }
+
+    // Get total vertices
+    public int getVertices() {
+        return vertices;
+    }
 }
